c163f15cbaab72eef98e1ea76b1eb6fa469c1863
RWitak <rafael.witak@gmail.com>
2022-12-21 15:58:44 +0100

    SDK 32->33, gradle->kts, upgrade dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,16 @@
+// Top-level build file where you can add configuration options common to all subprojects/modules.
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
+    }
+}
+
+plugins {
+    id("com.android.application") version "7.3.0" apply false
+    id("com.android.library") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.21" apply false
+}
+
+tasks.register("clean") {
+    delete(rootProject.buildDir)
+}
